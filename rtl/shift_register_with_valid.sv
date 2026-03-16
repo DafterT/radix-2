ce9e3f8991b987393570360368fd38505a2d5b40
Dafter <dafterplay@mail.ru>
2026-03-16 11:42:33 +0300

    add shift_register_with_valid

---
+++ rtl/shift_register_with_valid.sv
@@ -0,0 +1,41 @@
+module shift_register_with_valid
+# (
+    parameter width = 8, depth = 8
+)
+(
+    input  logic              clk,
+    input  logic              rst,
+
+    input  logic              in_vld,
+    input  logic              [width - 1:0] in_data,
+
+    output logic              out_vld,
+    output logic              [width - 1:0] out_data
+);
+
+    logic [width - 1:0] data [0:depth - 1];
+    logic valid [0:depth - 1];
+
+    always_ff @ (posedge clk)
+        if (rst) begin
+            for (int i = 0; i < depth; i++) begin
+                data[i] <= '0;
+                valid[i] <= '0;
+            end
+        end
+        else
+        begin
+            valid[0] <= in_vld;
+            if (in_vld)
+                data [0] <= in_data;
+
+            for (int i = 1; i < depth; i ++) begin
+                valid[i] <= valid[i - 1];
+                if (valid[i - 1])
+                    data [i] <= data [i - 1];
+            end
+        end
+
+    assign out_data = data [depth - 1];
+    assign out_vld = valid [depth - 1];
+endmodule
