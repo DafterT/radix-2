8bc9268b8bff1f0f2df2618e4ea7ea984cc1956b
Dafter <dafterplay@mail.ru>
2026-03-30 15:30:34 +0300

    change parametr name to uppercase

--- rtl/shift_register.sv
+++ rtl/shift_register.sv
@@ -2,38 +2,38 @@
 
 module shift_register
 #(
-    parameter int width = 8,
-    parameter int depth = 8
+    parameter int WIDTH = 8,
+    parameter int DEPTH = 8
 )
 (
     input  logic               clk,
     input  logic               rst,
-    input  logic [width - 1:0] in_data,
-    output logic [width - 1:0] out_data
+    input  logic [WIDTH - 1:0] in_data,
+    output logic [WIDTH - 1:0] out_data
 );
 
     initial begin
-        if (width <= 0)
-            $fatal(1, "shift_register: width must be > 0");
+        if (WIDTH <= 0)
+            $fatal(1, "shift_register: WIDTH must be > 0");
 
-        if (depth <= 0)
-            $fatal(1, "shift_register: depth must be > 0");
+        if (DEPTH <= 0)
+            $fatal(1, "shift_register: DEPTH must be > 0");
     end
 
-    logic [width - 1:0] data_q [0:depth - 1];
+    logic [WIDTH - 1:0] data_q [0:DEPTH - 1];
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            for (int i = 0; i < depth; i++)
+            for (int i = 0; i < DEPTH; i++)
                 data_q[i] <= '0;
         end else begin
             data_q[0] <= in_data;
 
-            for (int i = 1; i < depth; i++)
+            for (int i = 1; i < DEPTH; i++)
                 data_q[i] <= data_q[i - 1];
         end
     end
 
-    assign out_data = data_q[depth - 1];
+    assign out_data = data_q[DEPTH - 1];
 
 endmodule
--- rtl/shift_register_with_valid.sv
+++ rtl/shift_register_with_valid.sv
@@ -2,41 +2,41 @@
 
 module shift_register_with_valid
 #(
-    parameter int width = 8,
-    parameter int depth = 8
+    parameter int WIDTH = 8,
+    parameter int DEPTH = 8
 )
 (
     input  logic               clk,
     input  logic               rst,
 
     input  logic               in_vld,
-    input  logic [width - 1:0] in_data,
+    input  logic [WIDTH - 1:0] in_data,
 
     output logic               out_vld,
-    output logic [width - 1:0] out_data
+    output logic [WIDTH - 1:0] out_data
 );
 
-    logic [width - 1:0] data_q [0:depth - 1];
-    logic [depth - 1:0] valid_q;
+    logic [WIDTH - 1:0] data_q [0:DEPTH - 1];
+    logic [DEPTH - 1:0] valid_q;
 
     initial begin
-        if (width <= 0)
-            $fatal(1, "shift_register_with_valid: width must be > 0");
+        if (WIDTH <= 0)
+            $fatal(1, "shift_register_with_valid: WIDTH must be > 0");
 
-        if (depth <= 0)
-            $fatal(1, "shift_register_with_valid: depth must be > 0");
+        if (DEPTH <= 0)
+            $fatal(1, "shift_register_with_valid: DEPTH must be > 0");
     end
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            for (int i = 0; i < depth; i++) begin
+            for (int i = 0; i < DEPTH; i++) begin
                 data_q[i] <= '0;
             end
         end else begin
             if (in_vld)
                 data_q[0] <= in_data;
 
-            for (int i = 1; i < depth; i++) begin
+            for (int i = 1; i < DEPTH; i++) begin
                 if (valid_q[i - 1])
                     data_q[i] <= data_q[i - 1];
             end
@@ -49,13 +49,13 @@ module shift_register_with_valid
         end else begin
             valid_q[0] <= in_vld;
 
-            for (int i = 1; i < depth; i++) begin
+            for (int i = 1; i < DEPTH; i++) begin
                 valid_q[i] <= valid_q[i - 1];
             end
         end
     end
 
-    assign out_data = data_q[depth - 1];
-    assign out_vld  = valid_q[depth - 1];
+    assign out_data = data_q[DEPTH - 1];
+    assign out_vld  = valid_q[DEPTH - 1];
 
 endmodule
