683d381646655adb8c0bd44accac02a5255fffba
Dafter <dafterplay@mail.ru>
2026-04-01 14:50:49 +0300

    add compare function

--- model/stage/tools/fft_stage_compare.c
+++ model/stage/tools/fft_stage_compare.c
@@ -3,7 +3,11 @@
 #endif
 
 #ifndef WHITE_NOISE_BACKOFF_DB
-#define WHITE_NOISE_BACKOFF_DB 10.0
+#define WHITE_NOISE_BACKOFF_DB 0.0
+#endif
+
+#ifndef STAGE_COMPARE_RUNS
+#define STAGE_COMPARE_RUNS 100
 #endif
 
 #include "../fixed/fft_stage_model_fixed.h"
@@ -14,42 +18,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define COMPARE_PAIR_COUNT (FFT_N / 2)
+#define STAGE_CALLS_PER_RUN (FFT_N / 2)
+#define INPUT_WIDTH 16
 
-/* ===== Comparison math ===== */
-
-static double compare_white_noise_backoff_linear(void) {
-    return pow(10.0, WHITE_NOISE_BACKOFF_DB / 20.0);
+static double compare_random_unit_sample(void) {
+    return -1.0 + (2.0 * (double)rand() / (double)RAND_MAX);
 }
 
-static int16_t compare_quantize_normalized_to_q16_0(
-    double normalized_noise,
-    double backoff_linear
-) {
-    double backed_off_noise = normalized_noise / backoff_linear;
-    long quantized = lround(backed_off_noise * (double)INT16_MAX);
+static int16_t compare_round_clip_q16_0(double value_q16_0) {
+    const long max_q16_0 = (1L << (INPUT_WIDTH - 1)) - 1L;
+    const long min_q16_0 = -(1L << (INPUT_WIDTH - 1));
+    long rounded = lround(value_q16_0);
 
-    if (quantized > INT16_MAX) {
-        quantized = INT16_MAX;
+    if (rounded > max_q16_0) {
+        rounded = max_q16_0;
     }
 
-    if (quantized < -INT16_MAX) {
-        quantized = -INT16_MAX;
+    if (rounded < min_q16_0) {
+        rounded = min_q16_0;
     }
 
-    return (int16_t)quantized;
+    return (int16_t)rounded;
 }
 
-static double_stage_cpx_t compare_q16_input_to_double(fixed_stage_cpx_q16_0_t value) {
-    double_stage_cpx_t result;
+static fixed_stage_cpx_q16_0_t compare_make_fixed_input(double input_scale) {
+    fixed_stage_cpx_q16_0_t sample;
 
-    result.re = (double)value.re;
-    result.im = (double)value.im;
+    sample.re = compare_round_clip_q16_0(compare_random_unit_sample() * input_scale);
+    sample.im = compare_round_clip_q16_0(compare_random_unit_sample() * input_scale);
 
-    return result;
+    return sample;
 }
 
-static double_stage_cpx_t compare_fixed_output_to_double(fixed_stage_cpx_q16_0_t value) {
+static double_stage_cpx_t compare_fixed_to_double(fixed_stage_cpx_q16_0_t value) {
     double_stage_cpx_t result;
 
     result.re = (double)value.re;
@@ -62,7 +63,6 @@ static double compare_cpx_power(double_stage_cpx_t value) {
     return (value.re * value.re) + (value.im * value.im);
 }
 
-// SQNR = 10 * log10(sum(|signal|^2) / sum(|error|^2)).
 static double compare_sqnr_db(double signal_power, double noise_power) {
     if (noise_power <= 0.0) {
         return INFINITY;
@@ -71,94 +71,83 @@ static double compare_sqnr_db(double signal_power, double noise_power) {
     return 10.0 * log10(signal_power / noise_power);
 }
 
-/* ===== Stimulus and formatted output ===== */
-
-static int16_t compare_white_noise_q16_0_sample(double backoff_linear) {
-    double normalized_noise = -1.0 + (2.0 * (double)rand() / (double)RAND_MAX);
-
-    return compare_quantize_normalized_to_q16_0(normalized_noise, backoff_linear);
+static void compare_print_fixed_cpx(const char *name, fixed_stage_cpx_q16_0_t value) {
+    printf("  %-9s re=%10d im=%10d\n", name, value.re, value.im);
 }
 
-static void compare_fill_white_noise_signal(
-    fixed_stage_cpx_q16_0_t signal[FFT_N],
-    double backoff_linear
-) {
-    for (int i = 0; i < FFT_N; ++i) {
-        signal[i].re = compare_white_noise_q16_0_sample(backoff_linear);
-        signal[i].im = compare_white_noise_q16_0_sample(backoff_linear);
-    }
-}
-
-static void compare_print_complex(const char *name, double_stage_cpx_t value) {
-    printf("  %s: re=% .12f, im=% .12f\n", name, value.re, value.im);
-}
-
-/* ===== Comparison runner ===== */
-
-static void compare_run(void) {
-    fixed_stage_model_t fixed_model = {0};
-    double_stage_model_t double_model = {0};
-    fixed_stage_cpx_q16_0_t signal[FFT_N];
-    double backoff_linear = compare_white_noise_backoff_linear();
-    double reference_power = 0.0;
-    double error_power = 0.0;
-
-    srand(1);
-    compare_fill_white_noise_signal(signal, backoff_linear);
-
-    printf(
-        "Stage model comparison, FFT_N=%d, backoff=%.2f dB, scale=1/%.6f\n\n",
-        FFT_N,
-        WHITE_NOISE_BACKOFF_DB,
-        backoff_linear
-    );
-
-    for (int pair_index = 0; pair_index < COMPARE_PAIR_COUNT; ++pair_index) {
-        int last_i = (pair_index == (COMPARE_PAIR_COUNT - 1));
-        fixed_stage_cpx_q16_0_t a_fixed = signal[2 * pair_index];
-        fixed_stage_cpx_q16_0_t b_fixed = signal[(2 * pair_index) + 1];
-        double_stage_cpx_t a_double = compare_q16_input_to_double(a_fixed);
-        double_stage_cpx_t b_double = compare_q16_input_to_double(b_fixed);
-        fixed_stage_output_t fixed_output = fixed_stage_step(&fixed_model, a_fixed, b_fixed, last_i);
-        double_stage_output_t reference_output = double_stage_step(&double_model, a_double, b_double, last_i);
-        double_stage_cpx_t fixed_a = compare_fixed_output_to_double(fixed_output.a);
-        double_stage_cpx_t fixed_b = compare_fixed_output_to_double(fixed_output.b);
-        double_stage_cpx_t error_a;
-        double_stage_cpx_t error_b;
-
-        // SQNR uses the ideal double-precision output as the signal and the
-        // fixed-point deviation from that output as the noise.
-        error_a.re = reference_output.a.re - fixed_a.re;
-        error_a.im = reference_output.a.im - fixed_a.im;
-        error_b.re = reference_output.b.re - fixed_b.re;
-        error_b.im = reference_output.b.im - fixed_b.im;
-
-        reference_power += compare_cpx_power(reference_output.a);
-        reference_power += compare_cpx_power(reference_output.b);
-
-        error_power += compare_cpx_power(error_a);
-        error_power += compare_cpx_power(error_b);
-
-        printf("call=%2d last_i=%d\n", pair_index, last_i);
-        compare_print_complex("fixed_a_o", fixed_a);
-        compare_print_complex("ref_a_o", reference_output.a);
-        compare_print_complex("err_a_o", error_a);
-        compare_print_complex("fixed_b_o", fixed_b);
-        compare_print_complex("ref_b_o", reference_output.b);
-        compare_print_complex("err_b_o", error_b);
-    }
-
-    printf("\nReference power = %.12f\n", reference_power);
-    printf("Error power     = %.12f\n", error_power);
-
-    if (error_power == 0.0) {
-        printf("SQNR = inf dB\n");
-    } else {
-        printf("SQNR = %.12f dB\n", compare_sqnr_db(reference_power, error_power));
-    }
+static void compare_print_double_cpx(const char *name, double_stage_cpx_t value) {
+    printf("  %-9s re=%10.3f im=%10.3f\n", name, value.re, value.im);
 }
 
 int main(void) {
-    compare_run();
+    const double max_q16_0 = (double)((1L << (INPUT_WIDTH - 1)) - 1L);
+    const double fullscale_power = 2.0 * max_q16_0 * max_q16_0;
+    const double backoff_ratio = pow(10.0, WHITE_NOISE_BACKOFF_DB / 10.0);
+    const double input_scale = sqrt(fullscale_power / backoff_ratio);
+    double sqnr_sum_db = 0.0;
+
+    srand(1);
+
+    printf("FFT_N              = %d\n", FFT_N);
+    printf("Runs               = %d\n", STAGE_COMPARE_RUNS);
+    printf("Calls per run      = %d\n", STAGE_CALLS_PER_RUN);
+    printf("Total calls        = %d\n", STAGE_CALLS_PER_RUN * STAGE_COMPARE_RUNS);
+    printf("Backoff            = %.3f dB\n", WHITE_NOISE_BACKOFF_DB);
+    printf("Input scale        = %.3f\n\n", input_scale);
+
+    for (int run = 0; run < STAGE_COMPARE_RUNS; ++run) {
+        fixed_stage_model_t fixed_model = {0};
+        double_stage_model_t double_model = {0};
+        double signal_power = 0.0;
+        double noise_power = 0.0;
+
+        printf("run=%d\n", run);
+
+        for (int call = 0; call < STAGE_CALLS_PER_RUN; ++call) {
+            int last_i = (call == (STAGE_CALLS_PER_RUN - 1));
+            fixed_stage_cpx_q16_0_t a_fixed = compare_make_fixed_input(input_scale);
+            fixed_stage_cpx_q16_0_t b_fixed = compare_make_fixed_input(input_scale);
+            double_stage_cpx_t a_double = compare_fixed_to_double(a_fixed);
+            double_stage_cpx_t b_double = compare_fixed_to_double(b_fixed);
+            fixed_stage_output_t fixed_out = fixed_stage_step(&fixed_model, a_fixed, b_fixed, last_i);
+            double_stage_output_t double_out = double_stage_step(&double_model, a_double, b_double, last_i);
+            double_stage_cpx_t fixed_a = compare_fixed_to_double(fixed_out.a);
+            double_stage_cpx_t fixed_b = compare_fixed_to_double(fixed_out.b);
+            double_stage_cpx_t err_a;
+            double_stage_cpx_t err_b;
+
+            err_a.re = double_out.a.re - fixed_a.re;
+            err_a.im = double_out.a.im - fixed_a.im;
+            err_b.re = double_out.b.re - fixed_b.re;
+            err_b.im = double_out.b.im - fixed_b.im;
+
+            signal_power += compare_cpx_power(double_out.a);
+            signal_power += compare_cpx_power(double_out.b);
+            noise_power += compare_cpx_power(err_a);
+            noise_power += compare_cpx_power(err_b);
+
+            printf(" call=%d last=%d\n", call, last_i);
+            compare_print_fixed_cpx("a_i", a_fixed);
+            compare_print_fixed_cpx("b_i", b_fixed);
+            compare_print_double_cpx("fixed_a_o", fixed_a);
+            compare_print_double_cpx("ref_a_o", double_out.a);
+            compare_print_double_cpx("err_a_o", err_a);
+            compare_print_double_cpx("fixed_b_o", fixed_b);
+            compare_print_double_cpx("ref_b_o", double_out.b);
+            compare_print_double_cpx("err_b_o", err_b);
+            printf("\n");
+        }
+
+        double run_sqnr_db = compare_sqnr_db(signal_power, noise_power);
+
+        sqnr_sum_db += run_sqnr_db;
+
+        printf(" run_signal_power = %.3f\n", signal_power);
+        printf(" run_error_power  = %.3f\n", noise_power);
+        printf(" run_sqnr         = %.3f dB\n\n", run_sqnr_db);
+    }
+
+    printf("Average SQNR       = %.3f dB\n", sqnr_sum_db / (double)STAGE_COMPARE_RUNS);
+
     return 0;
 }
