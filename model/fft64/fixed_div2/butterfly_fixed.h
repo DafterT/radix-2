df72d2aed0a99b22bfd3b135ddd68bafbd4cb651
Dafter <dafterplay@mail.ru>
2026-04-14 20:10:20 +0300

    add fft64 div 2 realization

---
+++ model/fft64/fixed_div2/butterfly_fixed.h
@@ -0,0 +1,13 @@
+#ifndef FFT64_BUTTERFLY_FIXED_H
+#define FFT64_BUTTERFLY_FIXED_H
+
+#include "model_fixed.h"
+
+void fft64_fixed_butterfly(
+    fft64_fixed_cpx_q16_0_t *top,
+    fft64_fixed_cpx_q16_0_t *bottom,
+    int twiddle_index,
+    int apply_div2
+);
+
+#endif
